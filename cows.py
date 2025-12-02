93e5921ef81efd5d95278978f0af68141ede2418
Maria Maksimova <drogzed@gmail.com>
2025-12-02 20:50:03 +0100

    small logic implemented

--- cows.py
+++ cows.py
@@ -12,7 +12,8 @@ def generate_num():
     return number
 
 
-print(generate_num())
+guessed_number = generate_num()
+print(guessed_number)
 
 
 def check_player_input(number_from_player):
@@ -21,10 +22,31 @@ def check_player_input(number_from_player):
         if len(number_from_player) != 4:
             return "Write 4-digits"
         else:
-            return number_from_player
+            checking = set()
+            for num in number_from_player:
+                checking.add(num)
+            if len(checking) != 4:
+                return "Write number without the same nums!"
+            else:
+                return number_from_player
+
     except ValueError:
         return "Write a number"
 
 
 player_num = input("please write your number: ")
 print(check_player_input(player_num))
+
+
+def counting_cows_and_bulls(comp_number, player_number):
+    cow_counter = 0
+    bull_counter = 0
+    for number in comp_number:
+        if number in player_number:
+            cow_counter = cow_counter + 1
+            if player_number.rindex(number) == comp_number.rindex(number):
+                bull_counter = bull_counter + 1
+    return f"So you have {cow_counter} cow(s) and {bull_counter} buuul(s)!"
+
+
+print(counting_cows_and_bulls(guessed_number, player_num))
