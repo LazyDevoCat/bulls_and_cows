ea13350eae14319b3f0d6ab546a57e2272aeaf80
Maria Maksimova <drogzed@gmail.com>
2025-12-02 19:55:03 +0100

    Add creating number from scratch again

--- cows.py
+++ cows.py
@@ -1,42 +1,13 @@
 import random
 
-COUNTER = 0
+number = str()
 
-rules = str('''
-==== The rules of game: ====
-Guessed 4 numbers. You need to guess it.
-You have 3 attempts.
-In case if some number in your inputted NUMBER is correct and in correct position you will receive message BULLS.
-In case if some number in your inputted NUMBER is correct but position is incorrect you will receive message COWS.
-HINT: first number couldn't be 0.
-Good luck!
-''')
-
-print(rules)
-
-number_one = str(random.randint(1, 9))
-number_two = str(random.randint(0, 9))
-number_three = str(random.randint(0, 9))
-number_four = str(random.randint(0, 9))
-number = str(number_one + number_two + number_three + number_four)
-
-# print(number)
-
-player_num = str(input("Your number is: "))
+while len(number) != 4:
+    new_num = str(random.randint(0, 9))
+    if new_num in number:
+        continue
+    else:
+        number = number + new_num
 
 
-def compare(number_from_player: str, known_number: str):
-	BULL = 0
-	COWS = 0
-	for number in range(len(known_number)):
-		if number_from_player[number] == known_number[number]:
-			BULL = BULL + 1
-		if number_from_player[number] in known_number and number_from_player[number] != known_number[number]:
-			COWS = COWS + 1
-	print(f"BULL is {BULL} and COWS is {COWS}")
-
-
-print(compare(player_num, number))
-
-
-
+print(number)
