a94e4f0f3d9d49a94e340a3795bce8676aac77ee
Maria Maksimova <drogzed@gmail.com>
2025-12-02 20:12:08 +0100

    player input

--- cows.py
+++ cows.py
@@ -1,13 +1,30 @@
 import random
 
-number = str()
 
-while len(number) != 4:
-    new_num = str(random.randint(0, 9))
-    if new_num in number:
-        continue
-    else:
-        number = number + new_num
+def generate_num():
+    number = str()
+    while len(number) != 4:
+        new_num = str(random.randint(0, 9))
+        if new_num in number:
+            continue
+        else:
+            number = number + new_num
+    return number
 
 
-print(number)
+print(generate_num())
+
+
+def check_player_input(number_from_player):
+    try:
+        int(number_from_player)
+        if len(number_from_player) != 4:
+            return "Write 4-digits"
+        else:
+            return number_from_player
+    except ValueError:
+        return "Write a number"
+
+
+player_num = input("please write your number: ")
+print(check_player_input(player_num))
---
+++ readme.md
@@ -0,0 +1,29 @@
+### Rules
+
+Secret Number:
+Each player thinks of a secret number consisting of 4 digits.
+
+All digits must be different.
+
+The number cannot start with 0 (optional rule, but common).
+
+Making a Guess:
+Players take turns trying to guess each other’s secret number.
+
+Feedback – Bulls and Cows:
+After a guess, the opponent gives feedback:
+
+Bull = a correct digit in the correct position
+
+Cow = a correct digit in the wrong position
+
+Example:
+Secret number: 4271
+Guess: 1234
+
+1 bull (the digit 2 is in the correct position)
+
+2 cows (4 and 1 exist in the number but in different positions)
+
+Winning the Game:
+The first player to guess the secret number exactly (4 bulls) wins.
